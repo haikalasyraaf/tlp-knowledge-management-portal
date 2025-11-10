edea6326c21626c23846b545272d0d8e67d6ce7f
Haikal Asyraaf <haikal@dataflows.co>
2025-11-11 06:31:41 +0800

    reduce password length

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -18,7 +18,7 @@ class PasswordController extends Controller
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
             'password' => ['required', 'confirmed',
-                Password::min(8) // Minimum 8 characters
+                Password::min(6) // Minimum 6 characters
                     ->letters()   // Must contain letters
                     ->mixedCase() // Must contain both uppercase and lowercase
                     ->numbers(),  // Must contain numbers
